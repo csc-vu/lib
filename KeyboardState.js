b81281fb9a6d1c283debbc0685bd6124b13da33f
mirela-damian <87018649+mirela-damian@users.noreply.github.com>
2025-07-30 13:56:12 -0400

    Update KeyboardState.js

--- KeyboardState.js
+++ KeyboardState.js
@@ -5,25 +5,21 @@ export default class KeyboardState {
     this.keyStates = {};
 
     window.addEventListener('keydown', (event) => {
-      this.keyStates[event.key.toLowerCase()] = true;
+      this.keyStates[event.key] = true; // Keep original casing
     });
 
     window.addEventListener('keyup', (event) => {
-      this.keyStates[event.key.toLowerCase()] = false;
+      this.keyStates[event.key] = false;
     });
   }
 
   // Returns true if the key was just pressed this frame
   down(key) {
-    return this.keyStates[key.toLowerCase()] === true;
+    return this.keyStates[key] === true;
   }
 
-  // Alias for continuous check
+ // Alias for continuous check
   pressed(key) {
     return this.down(key);
-  }
-
-  update() {
-    // You can extend this if you want to debounce key transitions, but not needed for simple use
   }
 }
